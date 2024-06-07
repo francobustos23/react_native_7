a2962f4e8d11ac8de625f76f697b387da8ecd767
francobustos23 <bustosfranco071@gmail.com>
2024-06-07 10:02:03 -0300

    Add routes

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" />
-      <Stack.Screen name="registro" options={{ title: 'Registrarse' }} />
+      <Stack.Screen name="crearTarea" options={{ title: 'Crear Tarea' }} />
     </Stack>
   );
 }
